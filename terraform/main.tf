124387d7c4b5e499dadb91a62ae20f311d29952e
roey132 <roey132@gmail.com>
2025-07-28 16:21:19 +0700

    fix main terraform. change git ignore to hide terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,41 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc_dhcp_options" "dns_resolver" {
+  domain_name_servers = ["AmazonProvidedDNS"]
 }
+
+resource "aws_vpc_dhcp_options_association" "dns_attach" {
+  vpc_id          = aws_vpc.main.id
+  dhcp_options_id = aws_vpc_dhcp_options.dns_resolver.id
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.50.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "olist-dwh-vpc"
+  }
+}
+
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.50.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.50.2.0/24"
+  availability_zone       = "${var.aws_region}b"
   map_public_ip_on_launch = true
 }
 
@@ -26,8 +50,13 @@ resource "aws_route" "public_internet_access" {
   gateway_id             = aws_internet_gateway.gw.id
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
 }
 
@@ -53,13 +82,13 @@ resource "aws_security_group" "rds_sg" {
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds-subnet-group"
-  subnet_ids = [aws_subnet.public.id]
+  subnet_ids = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 }
 
 resource "aws_db_instance" "postgres" {
   identifier              = var.db_identifier
   engine                  = "postgres"
-  engine_version          = "15.4"
+  engine_version          = "14.17"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   db_name                 = var.db_name
