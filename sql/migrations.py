938aa3bca4dc9770002d82089de18e7032674c25
roey132 <roey132@gmail.com>
2025-07-28 17:38:08 +0700

    fix migrations script

--- sql/migrations.py
+++ sql/migrations.py
@@ -4,11 +4,18 @@ from yoyo import read_migrations, get_backend
 
 load_dotenv(".env")
 
+print(os.getenv("DB_USER"))
+print(os.getenv("DB_PASSWORD"))
+print(os.getenv("DB_HOST"))
+print(os.getenv("DB_PORT"))
+print(os.getenv("DB_NAME"))
+
 backend = get_backend(
     f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
 
-migrations = read_migrations("migrations/")
+migrations = read_migrations("sql/migrations/")
+print(migrations)
 with backend.lock():
     backend.apply_migrations(backend.to_apply(migrations))
